abe6851fa51205f321f96c78fc958469249c030b
Piotr Dziedzina <p.dziedzina@o2.pl>
2023-06-02 18:16:59 +0200

    updated styles and message

--- src/components/Warning.tsx
+++ src/components/Warning.tsx
@@ -4,8 +4,8 @@ import { Button } from '.'
 const Warning: React.FC<WarningProps> = ({ closeMsg }) => {
 	return (
 		<div className='absolute z-50 w-3/4 h-40 mx-[12.5%] md:w-1/3 bg-white mt-1/3 border-purple-500 md:mx-[calc(100%/3)] border-2 rounded-lg'>
-			<p className='text-center mt-[4.5em]'>
-				Please fill 
+			<p className='mt-2 ml-2'>
+				Please fill all required fields. Remember, if you want to submit your phone number, you must choose proper dialing code.
 			</p>
 			<div className='absolute right-2 bottom-2'>
 				<Button btnText='ok' handleClick={() => { closeMsg(true) }} />
