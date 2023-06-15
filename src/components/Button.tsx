ae488e90c7b64aad96e566803be92bb1351a33d7
Piotr Dziedzina <p.dziedzina@o2.pl>
2023-06-15 16:11:36 +0200

    added id prop to btn, changed its interface

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button: React.FC<ButtonProps> = ({ btnText, handleClick }) => {
+const Button: React.FC<ButtonProps> = ({ btnText, handleClick, id }) => {
 	
 	return (
 		<>
@@ -8,6 +8,7 @@ const Button: React.FC<ButtonProps> = ({ btnText, handleClick }) => {
 				type='button'
 				className='px-4 py-1 bg-purple-100 text-blue-950 border-2 border-purple-700 rounded-md w-[100px] uppercase'
 				onClick={handleClick}
+				id={id}
 			>{btnText}</button>
 		</>
 	)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 interface HeaderProps {
 	setRenderHome: React.Dispatch<React.SetStateAction<boolean>>;
-	isValid: boolean
+	isValid: boolean;
 }
 
 interface TextAreaElement {
@@ -41,7 +41,7 @@ interface Country {
 		root: string;
 		suffix: string;
 	};
-	altSpellings: string[]
+	altSpellings: string[];
 }
 
 interface ContactInitialState {
@@ -65,12 +65,13 @@ interface ContactPayload {
 }
 
 interface ButtonProps {
-	btnText: string
-	handleClick: () => void
+	btnText: string;
+	handleClick: (ev?) => void;
+	id?: string
 }
 
 interface ContactProps {
-	isValid: boolean
+	isValid: boolean;
 	setIsValid: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
@@ -80,16 +81,149 @@ interface WarningProps {
 
 interface LocationInitialState {
 	userLocation: {
-		status,
-		results
+		status;
+		result: {
+			timezone: Timezone;
+			components: Components;
+			formatted: string;
+		};
 	};
-	userCoordinates: {
-		latitude: number;
-		longitude: number;
-	};
+	userCoordinates: Coords 
 	isLoading: boolean;
+	searchedLocation: string;
 }
 
-// interface WeatherInitialState {
+interface Coords {
+	latitude: number | undefined;
+	longitude: number | undefined;
+}
 
+interface Timezone {
+	name: string;
+	offset_sec: number;
+	short_name: string;
+}
+
+interface Components {
+	city: string | undefined;
+	city_district: string | undefined;
+	county: string | undefined;
+	continent: string;
+	country: string;
+	country_code: string;
+	house_number: string | undefined;
+	postcode: string;
+	road: string;
+	village: string | undefined;
+	state: string | undefined;
+}
+
+interface WeatherInitialState {
+	isLoadingCurr: boolean;
+	isLoadingForecast: boolean;
+	currentWeather: CurrentWeather | undefined;
+	weatherForecast: ForecastWeather | undefined;
+}
+
+interface CurrentWeather {
+	coord: { lat: number; lon: number };
+	weather: Weather[];
+	base: string;
+	main: Main;
+	visibility: number;
+	wind: Wind;
+	rain?: { "1h": number | undefined; "3h": number | undefined };
+	snow?: { "1h": number | undefined; "3h": number | undefined };
+	clouds: { all: number };
+	dt: number;
+	sys: Sys;
+	timezone: number;
+	id: number;
+	name: string;
+	cod: number;
+}
+
+interface Sys {
+	type: number;
+	id: number;
+	country: string;
+	sunrise: number;
+	sunset: number;
+}
+
+interface ForecastWeather {
+	cod: string;
+	message: number;
+	cnt: number;
+	list: Array<ListItem>;
+	city: City;
+}
+
+interface ListItem {
+	dt: number;
+	main: Main;
+	weather: Weather[];
+	clouds: { all: number };
+	wind: Wind;
+	visibility: number;
+	pop: number;
+	rain?: { "3h": number };
+	snow?: { "3h": number };
+	sys: { pod: string };
+	dt_txt: string;
+}
+
+interface Main {
+	temp: number;
+	feels_like: number;
+	temp_min: number;
+	temp_max: number;
+	pressure: number;
+	sea_level: number;
+	grnd_level: number;
+	humidity: number;
+	temp_kf: number;
+}
+
+interface Weather {
+	id: number;
+	main: string;
+	description: string;
+	icon: string;
+}
+
+interface Wind {
+	speed: number;
+	deg: number;
+	gust: number;
+}
+
+type City = {
+	id: number;
+	name: string;
+	coord: {
+		lat: number;
+		lon: number;
+	};
+	country: string;
+	population: number;
+	timezone: number;
+	sunrise: number;
+	sunset: number;
+};
+
+interface ListItemProps {
+	text: JSX.Element
+}
+
+interface LeafletMapProps {
+	coords: Coords 
+}
+
+// interface WeatherForecastProps {
+// 	// data: ForecastWeather
 // }
+
+interface DailyForecastProps {
+	data: ListItem
+}
