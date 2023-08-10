135af7286d128971390d981a84f0f14318789e06
Piotr Dziedzina <p.dziedzina@o2.pl>
2023-08-10 12:41:18 +0200

    added reducers to store

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import contactReducer from '@/redux/features/contact/contactSlice'
+import contactReducer from "@/redux/features/contact/contactSlice";
+import userReducer from "@/redux/features/user/userSlice"
+import searchLocationReducer from "@/redux/features/user/searched-location/searchedLocationSlice"
+
 export const store = configureStore({
 	reducer: {
-		contact: contactReducer
+		contact: contactReducer,
+		user: userReducer,
+		searchedLocation: searchLocationReducer,
 	},
-	devTools: process.env.NODE_ENV !== 'production'
-})
+	devTools: process.env.NODE_ENV !== "production",
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
